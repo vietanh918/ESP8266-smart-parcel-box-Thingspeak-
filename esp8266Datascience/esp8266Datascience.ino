dd0841717479b455ee1477e7973ee6ef5050bef6
Viet Anh Le <vietanh.le918@gmail.com>
2020-01-17 22:17:56 +0100

    Added second servo (latch lock)

--- esp8266Datascience/esp8266Datascience.ino
+++ esp8266Datascience/esp8266Datascience.ino
@@ -4,8 +4,6 @@
 #include <Servo.h>
 
 /*-------------------------BEGIN WIFI---------------------*/
-//const char* ssid = "Tesla IoT";
-//const char* password = "";
 const char* ssid = "";
 const char* password = "";
 WiFiClient client;
@@ -17,9 +15,10 @@ const char * myWriteAPIKey = ""; // API Key
 /*---------------------------END THINGSPEAK-----------------------*/
 
 Servo servo;
+Servo servoLatchLock;
 int angle = 0;
-int val;
-const int pinSwitch = 12; //Pin reed switch
+int angleSLL = 0;
+const int pinSwitch = 4; //Pin reed switch
 int switchState = 0;
 
 void setup()
@@ -29,22 +28,43 @@ void setup()
   WiFi.begin(ssid, password);
   ThingSpeak.begin(client);
   servo.attach(5);
-  servo.write(0);
+  servoLatchLock.attach(16);
+  servo.write(0); // open after initialization
+  servoLatchLock.write(180); //close after initialization
   pinMode(pinSwitch, INPUT);
 }
 
 void loop()
 {
-  StatoSwitch = digitalRead(pinSwitch);
+  switchState = digitalRead(pinSwitch);
   Serial.println(switchState);
   ThingSpeak.writeField(myChannelNumber, 1, switchState, myWriteAPIKey);
-  if (switchState == HIGH /*&& (gewicht > 0)*/)
+
+  //  if (full then keep closed and send message full (led which shows its full) {
+  //    servo.write(0);
+  //  }
+
+  //  else if(){ //If something stuck then keep locks closed
+  //    servo.write(0);
+  //  }
+
+  if (switchState == 1 /*&& (gewicht > 0)*/) //If door is closed and weight > 0 then drop parcel
   {
     servo.write(180);
+    delay(500); //Change to millis later
+    servoLatchLock.write(0);
+
   }
-  else 
+//  else if (switchState == 0 /* && (gewicht == 0)*/) { //If door is open and weight == 0 then auto close door or give message
+//    //Close the door!! OR close it automatic with another stepper motor
+//    return;
+//  }
+
+  else
   {
-        servo.write(0);
+    servoLatchLock.write(180);
+    delay(500);
+    servo.write(0);
   }
 
 }
